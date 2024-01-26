387f581c6318a201b8f8b676acf1334531cf6b08
Julien Lach <leadedchamps@gmail.com>
2024-01-26 14:03:03 +0100

    PDO exercice OK

--- PDO_exercice/index.php
+++ PDO_exercice/index.php
@@ -35,7 +35,9 @@ $pdo = new PDO($dsn, $user, $password);
 // FETCH Tous les posts
 
 // User input
-$author = 'Bob';
+// $author = 'bob';
+// $is_published = true;
+// $id = 1;
 
 // Paramètres positionels pour récupérer les posts dans la BDD
 // $sql = 'SELECT * FROM posts WHERE author = ?';
@@ -54,14 +56,77 @@ $author = 'Bob';
 
 
 // Paramètres nommés
-$sql = 'SELECT * FROM posts WHERE author = :author';
+// $sql = 'SELECT * FROM posts WHERE author = :author && is_published = :is_published';
+// $statement = $pdo->prepare($sql);
+// $statement->execute(['author' => $author, 'is_published' => $is_published]); 
+// $posts = $statement->fetchAll(PDO::FETCH_OBJ);
+
+// foreach($posts as $post) {
+//     echo $post->title . "<br>";
+//     echo $post->body . "<br>"; // afficher aussi le body field dans la BDD
+//     echo "<br>";
+// }
+
+
+// Fetch un seul post
+// $sql = 'SELECT * FROM posts WHERE id = :id';
+// $statement = $pdo->prepare($sql);
+// $statement->execute(['id' => $id]); 
+// $post = $statement->fetch(PDO::FETCH_OBJ);
+// echo $post->title;
+// echo $post->body;
+
+
+// GET ROW COUNT
+// $statement = $pdo->prepare(('SELECT * FROM posts WHERE author = ?'));
+// $statement->execute([$author]);
+// $postCount = $statement->rowCount();
+
+// echo $postCount;
+
+
+// INSERER DES DONNEES
+// $title = 'Post 5';
+// $body = "Fixa inpenderet comes uno inopia Honoratus et intempestivam efferatus Antiochensis intempestivam Antiochensis ei ni elogio.";
+// $author = "Bill";
+
+// $sql = 'INSERT INTO posts(title, body, author) VALUE(:title, :body, :author);';
+// $statement = $pdo->prepare($sql);
+// $statement->execute(['title' => $title, 'body' => $body, 'author' => $author]);
+// echo 'Post Added';
+
+
+// METTRE A JOUR DES DONNEES
+// $id = 1;
+// $body = "Updated post Fixa inpenderet comes uno et intempestivam efferatus Antiochensis intempestivam Antiochensis ei ni elogio.";
+
+// $sql = 'UPDATE posts SET body = :body WHERE id = :id;';
+// $statement = $pdo->prepare($sql);
+// $statement->execute(['body' => $body, 'id' => $id]);
+// echo 'Post Updated';
+
+// SUPPRIMER DES DONNEES
+// $id = 3;
+// $body = "Updated post Fixa inpenderet comes uno et intempestivam efferatus Antiochensis intempestivam Antiochensis ei ni elogio.";
+
+// $sql = 'DELETE FROM posts WHERE id = :id;';
+// $statement = $pdo->prepare($sql);
+// $statement->execute(['id' => $id]);
+// echo 'Post Deleted';
+
+
+// CHERCHER DES DONNEES
+$search = "%Po%";
+$sql = 'SELECT * FROM posts WHERE title LIKE ?;';
 $statement = $pdo->prepare($sql);
-$statement->execute(['author' => $author]); 
+$statement->execute([$search]);
 $posts = $statement->fetchAll(PDO::FETCH_OBJ);
 
 foreach($posts as $post) {
-    echo $post->title . "<br>";
-    echo $post->body . "<br>"; // afficher aussi le body field dans la BDD
-    echo "<br>";
+    echo $post->title .'<br>';
 }
-?>
+
+?> 
+
+
+
