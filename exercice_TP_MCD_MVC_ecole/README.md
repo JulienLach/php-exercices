ef32e1e72591e9c2cd60f3d855390bea50f66b7d
Julien Lach <leadedchamps@gmail.com>
2024-02-13 13:16:23 +0100

    exo TP MCD et MVC

---
+++ exercice_TP_MCD_MVC_ecole/README.md
@@ -0,0 +1,9 @@
+## Exercice TP
+
+- Suite à la création du MCD et la création de la base du phpMyAdmin, réussir à afficher les donnée des notes atttribuées aux élèves
+
+- Créer la PDO
+
+- Créer le modèle de récupération des donneés des notes
+
+- Créer le contrôleur et afficher la vue des notes
---
+++ exercice_TP_MCD_MVC_ecole/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP MCD & MVC école</title>
+</head>
+<body>
+    <h3>Notes élèves</h3>
+    <nav>
+        <ul>
+            <li><a href="controller.php">Liste des notes</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
