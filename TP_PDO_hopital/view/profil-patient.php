814823596f84a564f8eba957f6077598454ba849
Julien Lach <leadedchamps@gmail.com>
2024-02-26 16:46:25 +0100

    ok

--- TP_PDO_hopital/view/profil-patient.php
+++ TP_PDO_hopital/view/profil-patient.php
@@ -7,7 +7,11 @@ require_once '../controller/profil-patient.php';
 <h3>Profil du patient</h3>
 <?php
 
-echo $patient['lastname'] . '<br>';
+echo "Nom : " . $patient['lastname'] . '<br>';
+echo "Prénom : " . $patient['firstname'] . '<br>';
+echo "Date de naissance : " .$patient['birthdate'] . '<br>';
+echo " Téléphone : " . $patient['phone'] . '<br>';
+echo "Email : " . $patient['mail'] . '<br>';
 
 
 ?>
